550021584e2c9ca0ba26c94f09ecea57b3360381
morbstura <153026144+morbstura@users.noreply.github.com>
2023-12-21 17:08:12 -0500

    updating file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+module.exports = defineConfig({ 
+  chromeWebSecurity: false,
+   e2e: {
+  setupNodeEvents(on, config){
+
+  }
+  
     },
-  },
-});
+   })
+
+
+
+   
+ 
+
---
+++ frames.spec.cy.js
@@ -0,0 +1,31 @@
+describe('template spec', () => {
+    it('verify if frame has content', () => {
+      cy.on('uncaught:exception', (err, runnable) => { 
+      return false
+      cy.visit('https://demoqa.com/frames')
+      cy.get('#frame2')
+      cy.contains('This is a sample page').should("exist")
+         .then(($iframe)=>{
+              const $body = $iframe.contents().find('body')
+         })
+        })
+
+    cy.wrap($body)
+      .find('sidebar').scrollTo('bottom')
+       .find('sidebar').scrollTo('top')
+         })
+          
+      it('verify that there is content in child frame',() => {
+        cy.on('uncaught:exception', (err, runnable) => { 
+            return false
+            cy.visit
+            cy.get('#frame1')
+            cy.contains('Child Iframe').should("exist")
+
+
+
+
+
+      })
+    })
+})
